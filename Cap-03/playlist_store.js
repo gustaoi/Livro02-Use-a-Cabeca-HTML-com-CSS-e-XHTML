a7156ef976e5ff452f4ce18ea0b32ccf4fe63a81
gustavo santos <110102661+gustaoi@users.noreply.github.com>
2023-05-15 17:56:48 -0300

    Store of playlist

---
+++ Cap-03/playlist_store.js
@@ -0,0 +1,20 @@
+/* Guardando as músicas */
+
+function init() {
+    var button = document.getElementById('iaddbutton');
+
+    button.onclick = handleButtonClick;
+    loadPlaylist();
+}
+
+function handleButtonClick() {
+    var textInput = document.getElementById("isongText");
+    var songName = textInput.value;
+
+    var li = document.createElement("li");
+    li.innerHTML = songName;
+
+    var ul = document.getElementById("iplaylist")
+    ul.appendChild(li);
+    save(songName)
+}
