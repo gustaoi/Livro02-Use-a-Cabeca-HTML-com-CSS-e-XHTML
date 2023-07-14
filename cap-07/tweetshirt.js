b726ced3c60c8f3522b4291b7b969da2116ebb06
Gustavo Santos <gustavojgsd@gmail.com>
2023-07-13 23:21:31 -0300

    :rocket: Cap-07 finished
    Cap-07 foi finalizado

--- cap-07/tweetshirt.js
+++ cap-07/tweetshirt.js
@@ -7,31 +7,34 @@ function previewHandler() {
     let canvas = document.getElementById("tshirtCanvas");
     let context = canvas.getContext("2d");
 
-    // Reseta o background do canvas
-    fillBackgroundColor(canvas, context);
-
     // teste para testar se seu browser suporta canvas
     if (canvas.getContext) {
 
+        // Reseta o background do canvas
+        fillBackgroundColor(canvas, context);
+
+        // Cria os Quadrados no canvas
+        let selectObj = document.getElementById('shape');
+        let index = selectObj.selectedIndex;
+        let shape = selectObj[index].value;
+
+        if (shape == "squares") {
+            for (let squares = 0; squares < 20; squares++) {
+                drawSquare(canvas, context);
+            }
+        } else if (shape === "circles") {
+            for (let circles = 0; circles < 20; circles++) {
+                drawCircles(canvas, context);
+            }
+        };
+
+        drawText(canvas, context);
+        drawBird(canvas, context);
     } else {
         alert("your browser don't support canvas")
     };
 
 
-    // Cria os Quadrados no canvas
-    let selectObj = document.getElementById('shape');
-    let index = selectObj.selectedIndex;
-    let shape = selectObj[index].value;
-
-    if (shape == "squares") {
-        for (let squares = 0; squares < 20; squares++) {
-            drawSquare(canvas, context);
-        }
-    } else if (shape === "circles") {
-        for (let circles = 0; circles < 20; circles++) {
-            drawCircles(canvas, context);
-        }
-    }
 }
 
 function fillBackgroundColor(canvas, context) {
@@ -70,7 +73,7 @@ function degreesToRadians(params) {
     return (params * Math.PI) / 180
 }
 
-// Make Circles in canv
+// Make Circles in Canva
 function drawCircles(canvas, context) {
     const x = Math.floor(Math.random() * canvas.width);
 
@@ -85,6 +88,42 @@ function drawCircles(canvas, context) {
     context.fill();
 }
 
+// Make Fonts
+
+function drawText(canvas, context) {
+    let selectObj = document.getElementById("foregroundColor");
+
+    let index = selectObj.selectedIndex;
+
+    const bgColor = selectObj[index].value;
+
+    context.fillStyle = bgColor;
+
+    context.font = "bold 1em sans-serif"
+
+    context.textAlign = "left";
+
+    context.fillText("I saw this tweet", 20, 40);
+
+    // Get the selected tweet from the tweets menu. Draw the tweet
+
+    selectObj = document.getElementById("tweets");
+
+    index = selectObj.selectedIndex;
+
+    const tweet = selectObj[index].value;
+
+    context.font = "italic 1.2em sans-serif";
+
+    context.fillText(tweet, 30, 100);
+
+    context.font = "bold 1em sans-serif";
+
+    context.textAlign = "right";
+
+    context.fillText("and all I got was this lousy-tshirt", canvas.width - 20, canvas.height - 40);
+};
+
 // API twitter
 
 let url = "http://localhost:5500/cap-07/tweetshirt.json";
@@ -96,26 +135,38 @@ request.open("GET", url);
 request.onload = function () {
     if (request.status == 200) {
         updatesTweets(request.responseText);
-    }
+    };
+    function updatesTweets(responseText) {
+
+        var tweetsSelections = document.getElementById("tweets");
+
+        var tweets = JSON.parse(responseText)
+
+        for (let i = 0; i < tweets.length; i++) {
+            var tweet = tweets[i];
+
+            var option = document.createElement("option");
+
+            option.text = tweet.text;
+
+            option.value = tweet.text.replace("\"", "'");
+
+            tweetsSelections.options.add(option)
+        }
+
+        tweetsSelections.selectedIndex = 0;
+    };
 };
+request.send(null);
 
-function updatesTweets(responseText) {
+// Desenhando a imagem do twitter
 
-    var tweetsSelections = document.getElementById("tweets");
+function drawBird(canvas, context) {
+    let twitterBird = new Image();
 
-    var tweets = JSON.parse(responseText)
+    twitterBird.src = "https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-logo-vector-png-clipart-1.png";
 
-    for (let i = 0; i < tweets.length; i++) {
-        var tweet = tweets[i];
-
-        var option = document.createElement("option");
-
-        option.text = tweet.text;
-
-        option.value = tweet.text.replace("\"", "'");
-
-        tweetsSelections.option.options.add(option)
-    }
-
-    tweetsSelections.selectedIndex = 0;
+    twitterBird.onload = function () {
+        context.drawImage(twitterBird, 20, 120, 70, 70);
+    };
 }
